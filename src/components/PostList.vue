c862753ab3e78d5c03e40d0c79eb01f7da9981ef
Roman-00 <rvasin00@gmail.com>
2021-08-29 10:35:10 +0600

    feat: app done a 70%

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,12 +1,14 @@
 <template>
     <div v-if="posts.length > 0">
         <h3>Post List</h3>
-        <post-item 
-            v-for="post in posts"
-            :post="post"
-            :key="post.id"
-            @remove="$emit('remove', post)"
-        />
+        <transition-group name="post-list">
+            <post-item 
+                v-for="post in posts"
+                :post="post"
+                :key="post.id"
+                @remove="$emit('remove', post)"
+            />
+        </transition-group>
     </div>
     <h2 v-else style="color: red">
         List Post Not Found
@@ -27,5 +29,16 @@ export default {
 </script>
 
 <style lang="sass" scoped>
-
+.post-list-item
+    display: inline-block
+    margin-right: 10px
+.post-list-enter-active,
+.post-list-leave-active
+    transition: all .5s ease
+.post-list-enter-from,
+.post-list-leave-to
+    opacity: 0
+    transform: translateY(30px)
+.post-list-move
+    transition: transform .4s ease
 </style>
---
+++ src/components/UI/MyDialog.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="dialog" v-if="show" @click="hideDialog">
+
+        <div class="dialog__content" @click.stop>
+            <slot></slot>
+        </div>
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'my-dialog',
+    props: {
+        show: {
+            type: Boolean,
+            default: false
+        }
+    },
+    methods: {
+        hideDialog(e) {
+            this.$emit('update:show', false);
+        }
+    }
+}
+</script>
+
+<style lang="sass">
+.dialog
+    position: fixed
+    top: 0
+    bottom: 0
+    right: 0
+    left: 0
+    display: flex
+    background: rgba(0, 0, 0, 0.5) 
+    &__content
+        min-width: 300px
+        min-height: 50px
+        margin: auto
+        padding: 20px
+        background-color: white
+        border-radius: 12px
+</style>
